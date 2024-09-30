88fb8b67ab4e0a7cf8c7d03b7e14313d7aa33d3b
Ihar-Batura <A17331582@gmail.com>
2024-09-30 19:07:25 +0300

    feat: add solution for cli env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = Object.keys(process.env).filter(
+    (envVariables) => envVariables.slice(0, 4) === 'RSS_'
+  );
+  let conclusion = '';
+  for (let i = 0; i < envVariables.length; i++) {
+    let envVar = `${envVariables[i]}=${process.env[envVariables[i]]}${
+      i + 1 === envVariables.length ? '' : '; '
+    }`;
+    conclusion += envVar;
+  }
+  console.log(conclusion);
 };
 
 parseEnv();
