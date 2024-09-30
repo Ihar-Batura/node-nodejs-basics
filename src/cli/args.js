432cef1334bbf6d27d4ba22107d4b99aae05c228
Ihar-Batura <A17331582@gmail.com>
2024-09-30 19:50:01 +0300

    feat: add solution for cli args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let stringArgs = '';
+  for (let i = 0; i < args.length; i += 2) {
+    let argNewFormat = `${args[i].slice(2)} is ${args[i + 1]}${
+      i + 2 === args.length ? '' : ', '
+    }`;
+    stringArgs += argNewFormat;
+  }
+  console.log(stringArgs);
 };
 
 parseArgs();
