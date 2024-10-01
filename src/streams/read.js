695744216a9dc336bd8229cf46341da29cf88eb2
Ihar-Batura <A17331582@gmail.com>
2024-10-01 13:15:33 +0300

    feat: add solution for stream read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+const readableStream = createReadStream(pathToFile);
+
 const read = async () => {
-    // Write your code here 
+  readableStream.on('data', (chunk) => {
+    console.log(chunk.toString());
+  });
 };
 
 await read();
+
+//fs.createReadStream() - используется для создания читаемого потока для чтения данных из файла
+
+// readable.pipe() - в потоке, доступном для чтения, используется для присоединения потока, доступного для записи, к потоку, доступному для чтения, чтобы он впоследствии переключился в режим потока, а затем передал все имеющиеся у него данные в присоединенный поток, доступный для записи.
