fbcd136071fc7c93cd56916e658c5ee55600f02a
Ihar-Batura <A17331582@gmail.com>
2024-10-01 15:42:30 +0300

    feat: add solution for compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import zlib from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+const pathToNewArchive = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const readFile = createReadStream(pathToFile);
+  const writeFile = createWriteStream(pathToNewArchive);
+  readFile.pipe(gzip).pipe(writeFile);
 };
 
 await compress();
